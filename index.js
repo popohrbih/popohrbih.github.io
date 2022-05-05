a1bb2cb2e7e33a85119399b62798efd180e0618a
popohrbih <104939010+popohrbih@users.noreply.github.com>
2022-05-05 14:16:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,111 @@
+var hrvatska = ['Županja','Babina Greda','Bošnjaci','Cerna','Drenovci','Đurići','Posavski Podgajci','Račinovci','Rajevo Selo','Gradište','Gunja','Štitar','Ljupina','Prvča',
+'Brodski Varoš','Slavonski Brod','Podvinje','Banovci','Bebrina','Dubočac','Kaniža','Stupnički Kuti','Šumeće','Zbjeg','Bukovlje','Vranovci','Davor','Orubica','Donji Andrijevci',
+'Sredanci','Staro Topolje','Donji Bogićevci','Dragalić','Gorice','Bicko Selo','Sapci','Trnjani','Zadubravlje','Donja Vrba','Gornja Vrba','Dubovac','Gornji Bogićevci',
+'Kosovac','Gundinci','Donja Bebrina','Gornja Bebrina','Klakar','Rušćica','Batrina','Magić Mala','Seoce','Siče','Čovac','Vrbovljani','Novi Grad','Oprisavci','Poljanci',
+'Prnjavor','Stružani','Svilaj','Trnjanski Kuti','Zoljani','Kujnik', 'Lužani', 'Malino','Oriovac', 'Pričac', 'Slavonski Kobaš', 'Živike','Tomica','Bartolovci','Gornji Andrijevci',
+'Gromačnik','Sibinj','Slobodnica','Jaruge','Sikirevci','Kruševica','Slavonski Šamac','Donji Varoš','Gornji Varoš','Gređani','Novi Varoš','Pivare','Stara Gradiška',
+'Uskoci','Donji Crnogovci','Komarnica','Laze','Staro Petrovo Selo','Štivica','Beravci','Divoševci','Kupina','Mala Kopanica','Velika Kopanica','Bodovaljci','Dolina',
+'Mačkovac','Savski Bok','Sičice','Visoka Greda','Vrbje','Bojna','Borovita','Brezovo Polje','Brubno','Buzeta','Mali Obljaj','Šaševa','Veliki Obljaj','Čukur','Hrvatska Kostajnica',
+'Panjani','Rausovac','Rosulje','Selište Kostajničko','Utolica','Donja Velešnja','Donji Kukuruzari','Gornja Velešnja','Gornji Kukuruzari','Buinja','Bansko Vrpolje',
+'Buinjski Riječani','Ćore','Divuša','Donja Oraovica','Donji Dobretin','Donji Javoranj','Donji Žirovac','Draškovac','Dvor','Glavičani','Golubovac Divuški','Gorička',
+'Gornja Oraovica','Gornji Dobretin','Gornji Javoranj','Gornji Žirovac','Grmušani','Hrtić','Javnica','Javornik','Jovac','Kepčije','Kobiljak','Komora','Kosna','Kotarani',
+'Kozibrod','Kuljani','Lotine','Ljubina','Majdan','Matijevići','Ostojići','Paukovac','Sočanica','Stanić Polje','Struga Banska','Šakanlije','Šegestin','Unčani','Vanići',
+'Volinja','Zakopa','Zamlača','Zrin','Zut','Baćin','Donji Cerovljani','Gornji Cerovljani','Hrvatska Dubica','Slabinja','Živaja','Drenov Bok','Jasenovac','Košutarica',
+'Krapje','Mlaka','Tanac','Uštica','Višnjica Uštička','Graboštani','Kostrići','Majur','Mračaj','Srednja Meminska','Stubalj','Veliko Krčevo','Bročice','Jazavica',
+'Novska','Paklenica','Plesmo','Rajić','Roždanik','Stara Subocka','Stari Grabovac','Voćarica','Slovinci','Šaš','Timarci','Crni Potok','Hrvatsko Selo','Katinovac',
+'Mala Vranovina','Pecka','Perna','Ponikvari','Staro Selo Topusko','Velika Vranovina','Vorkapić','Čamerovac','Donji Furjan','Gornji Furjan','Kosa','Salopek Luke',
+'Sastavak','Zapoljak','Batnoga','Begovo Brdo','Bilo','BogovoIja','Buhača','Cetingrad','Cetinski Varoš','Delić Poljana','Donja Žrvnica','Đurin Potok','Gornja Žrvnica',
+'Grabarska','Kapljuv','Kestenje','Komesarac','Kruškovača','Kuk','Luke','Maljevac','Maljevačko Selište','Pašin Potok','Podcetin','Polojski Varoš','Ruševica',
+'Sadikovac','Srednje Selo','Strmačka','Šiljkovača','Trnovi','Basara','Brezovac','Drežnik Grad','Grabovac','Jamarje','Koranski Lug','Kordunski Ljeskovac','Lipovac',
+'Lipovača','Mašvina','Nova Kršlja','Sadilovac','Stara Kršlja','Donja Brusovača','Gejkovac','Jagrovac','Mracelj','Selakova Poljana','Svinica Krstinjska','Široka Rijeka',
+'Štakorovica','Birovača','Boričevac','Bušević','Dobroselo','Doljani','Donji Lapac','Donji Štrbci','Gajine','Gornji Lapac','Gornji Štrbci','Kestenovac','Kruge','Melinovac',
+'Mišljenovac','Nebljusi','Oraovac','Bjelopolje','Čujića Krčevina','Donji Vaganac','Gornji Vaganac','Gradina Korenička','Jasikovac','Kapela Korenička','Kompolje Koreničko',
+'Korenica','Ličko Petrovo Selo','Mihaljevac','Novo Selo Koreničko','Plitvički Ljeskovac','Ponor Korenički','Prijeboj','Rešetar','Rudanovac','Smoljanac','Šeganovac','Tuk Bjelopoljski',
+'Zaklopača','Željava','Frkašić','Klašnjica','Pecane','Vedašić','Begluci','Brotnja','Dabašnica','Drenovac Osredački','Dugopolje','Kaldrma','Kunovac Kupirovački','Kupirovo',
+'Neteka','Osredci','Prljevo','Rastičevo','Srb','Donja Suvaja','Gornja Suvaja','Tiškovac Lički','Zaklopac','Golubić','Kninsko polje','Plavno','Polača','Strmica','Vrpolje',
+'Cetina','Civljane','Kijevo','Donji Vinjani','Glavina Donja','Glavina Gornja','Gornji Vinjani','Imotski','Medvidovića Draga','Bajagić','Gljev','Obrovac Sinjski',
+'Kamensko','Ljut','Rože','Tijarica','Voštane','Banja','Draževitići','Dusina','Kotezi','Kozica','Mijaca','Orah','Podprolog','Poljica Kozička','Prapatnice','Ravča',
+'Stilja','Umčani','Veliki Prolog','Vina','Vrgorac','Zavojane','Garjak','Ježević','Koljane','Vinalić','Aržano','Dobranje','Svib','Dabar','Donji Bitelić','Gornji Bitelić',
+'Rumin','Vučipolje','Dolića Draga','Lokvičići','Lovreć','Studenci','Gala','Korita','Ovrlje','Ruda','Grubine','Hršćevani','Kamenmost','Podbablje Gornje','Donji Proložac',
+'Gornji Proložac','Postranje','Ričice','Šumet','Podosoje','Runović','Slivno','Zmijavci','Bosanka','Brsečine','Čajkovica','Čajkovići','Donje Obuljeno','Dubravica',
+'Dubrovnik','Gornje Obuljeno','Gromača','Kliševo','Knežica','Komolac','Lozica','Ljubač','Mokošica','Mravinjac','Mrčevo','Nova Mokošica','Orašac','Osojnik','Petrovo Selo',
+'Pobrežje','Prijevoj','Rožat','Sustjepan','Šumet','Trsteno','Zaton','Dubravica','Glušci','Metković','Prud','Vid','Plina jezero','Staševica','Banici','Čepikuće',
+'Imotica','Kručica','Lisac','Majkovi','Mravinca','Ošlje','Podgora','Podimoć','Slano','Smokovljani','Stupa','Štedrica','Točionik','Topolo','Trnova','Trnovica','Visočani',
+'Brotnice','Cavtat','Ćilipi','Drvenik','Duba Konavoska','Dubravka','Dunave','Gabrili','Jesenice','Kuna Konavoska','Lovorno','Ljuta','Mihanići','Močići','Palje Brdo',
+'Pridvorje','Stravča','Šilješki','Uskoplje','Vodovađa','Zastolje','Zvekovica','Borovci','Desne','Krvavac II','Kula Norinska','Matijevići','Momići','Nova Sela',
+'Podrujnica','Brečići','Dubrave','Kobiljača','Mali Prolog','Otrić-Seoci','Pozla Gora','Duboka','Klek','Komarna','Lovorje','Pižinovac','Podgradina','Raba','Slivno Ravno',
+'Tuštevac','Zavala','Boljenovići','Brijesta','Česvinica','Dančanje','Duba Stonska','Dubrava','Hodilje','Luka','Metohija','Putniković','Sparagovići','Zabrđe','Zaton Doli',
+'Badžula','Bijeli Vir','Dobranje','Mislina','Mlinište','Vidonje','Brašina','Buići','Čelopeci','Čibača','Donji Brgat','Gornji Brgat','Grbavac','Kupari','Makoše','Mandaljena',
+'Martinovići','Mlini','Petrača','Plat','Soline','Srebreno','Zavrelje'];
+
+
+var bih = ['Bukovica Donja','Crnjelevo Donje','Crnjelevo Gornje','Dragaljevac Donji','Ostojićevo','Brčko','Breznik','Brezovo Polje','Brezovo Polje Selo','Brod',
+'Čađavac','Donji Rahić','Gorice','Grbavica','Gredice','Krepšić','Marković Polje','Omerbegovača','Popovo Polje','Potočari','Sandići','Slijepčevići','Stanovi',
+'Trnjaci','Vukšić Donji','Bukova Greda','Donja Mahala','Jenjić Kopanice','Kostrč','Lepenica','Matici','Orašje','Tolisa','Ugljara','Vidovice','Šamac','Brvnik',
+'Donji Hasić','Novo Selo','Pisari','Škarić','Tišina','Bazik','Šamac','Domaljevac','Grebnice','Tišina','Donja Dubica','Donji Svilaj','Gornja Dubica','Gornji Svilaj',
+'Novi Grad','Osječak','Posavska Mahala','Prud','Vojskova','Vrbovac','Donja Barica','Brod','Brisnica Mala','Brisnica Velika','Donji Klakar','Gornji Klakar','Donje Kolibe',
+'Gornje Kolibe','Korače','Kričanovo','Liješće','Donja Močila','Gornja Močila','Novo Selo','Sijekovac','Unka','Vinska','Donja Vrela','Begluci','Bijelo Brdo','Dubočac',
+'Kostreš','Kuljenovci','Miškovci','Pjeva lovac','Trstenci','Bajinici','Bardača','Kobaš','Brusnik','Dugo Polje','Gaj','Glamočani','Inađol','Kaoci','Korovi','Nova Ves',
+'Resavac','Seferovci','Sitneši','Srbac','Vlaknica','Selo Srbac','Mali Sitneši','Donja Lepenica','Gornja Lepenica','Donji Srđevići','Gornji Srđevići','Povelići',
+'Prijebljezi','Crnaja','Brezovljani','Nožičko','Stari Martinac','Selište','Ćukali','Iličani','Razboj Župski','Kukulje','Lilić','Razboj Ljevčanski','Donji Kladari',
+'Gornji Kladari','Bistrica','Bok Jankovac','Gradiška','Breštovčina','Bukovac','Čatrnja','Donja Dolina','Gašnica','Gornja Dolina','Gornja Lipovača','Greda','Kozinci',
+'Krušik','Laminici Brezici','Laminici Dubrave','Laminci Jaružani','Laminci Sređani','Liskovac','Mačkovac','Mokrice','Novo Selo','Orahova','Orubica','Vrbaška','Žeravice',
+'Babinac','Bačvani','Kozarska Dubica','Demirovac','Dizdarlije','Donja Slabinja','Draksenić','Gradina Donja','Hadžibajir','Jasenje','Johova','Jošik','Klekovci','Komlenac',
+'Međeđa','Mrazovci','Novoselci','Parnice','Pobrđani','Sključani','Suvaja','Ševarlije','Tuključani','Verija','Vrioci','Blatna','Kostajnica','Cerovica','Crna Rijeka',
+'Čađavica Donja','Čađavica Gornja','Čađavica Srednja','Dobrljin','Donje Vodičevo','Donji Rakani','Gornje Vodičevo','Gornji Rakani','Grdanovac','Gumnjani','Kuljani',
+'Mala Žuljevica','Maslovare','Matavazi','Mazić','Mrakodol','Petrinja','Poljavnice','Ravnice','Rudice','Tavija','Velika Žuljevica','Banjanu','Baštra','Ivanjska','Bosanska Bojna',
+'Bukovlje','Donja Vidovska','Elezovići','Glavica','Glinica','Gornja Vidovska','Grabovac','Gradina','Grahovo','Johovica','Kudići','Miljkovići','Nepeke','Podzvizd',
+'Poljana','Polje','Ponikve','Rajnovac','Stabandža','Šiljkovača','Šmrekovac','Trn','Trnovi','Velika Kladuša','Vrnograč','Zagrad','Zborište','Mala Kladuša','Slapnica',
+'Crvarevac','Kumarica','Bukovica','Crnaja','Čehići','Hadžin Potok','Krivaja','Mutnik','Pjanići','Rujnica','Šturlić','Šturlićka Platnica','Tržac','Tržačka Platnica',
+'Tržačka Raštela','Urga','Liskovac','Čajići','Rajak','Baljevac','Bihać','Bugar','Ćukovi','Doljani','Donja Gata','Izačić','Kalati','Klisa','Klokot','Kulen Vakuf','Lohovo',
+'Lohovska Brda','Mala Peća','Mali Skočaj','Međudražje','Muslići','Orošac','Ostrovica','Papari','Praščijak','Prnjavor','Račić','Rajinovci','Ripač','Turija','Velika gata',
+'Veliki Skočaj','Veliki Stijenjani','Vikići','Vrsta','Zavalje','Zlopoljac','Bosanski Osredci','Gornji Tiškovac','Mali Cvijetnić','Malo Očijevo','Martin Brod','Očigrije',
+'Palučci','Trubar','Veliki Cvijetnić','Veliko Očijevo','Boboljusci','Hrnjadi','Bosansko Grahovo','Donji Kazanci','Donji Tiškovac','Duler','Mračaj','Pečenci','Stožišta',
+'Ugarci','Uništa','Zaseok','Krnete','Resanovci','Čaprazlije','Čuklić','Ćosanlije','Donji Rujani','Gornji Rujani','Gubin','Lipa','Listani','Odžak','Orguz','Podgradina',
+'Prolog','Provo','Tribić','Vržerala','Gornja Prisika','Kazaginac','Liskovača','Mijakovo Pije','Pasić','Rašeljke','Renići','Rošnjače','Vinica','Zaljut','Batin','Čitluk',
+'Gradac','Osoje','Podbila','Posušje','Vinjani','Zavelim','Vir','Zagorje','Drinovci','Gorica','Jabuka','Puteševica','Sovici','Crveni Grm','Grab','Grabovnik','Greda',
+'Hardomilje','Hrašljani','Humac','Kašče','Lisice','Orahovlje','Otok','Stubica','Teskera','Vašarovići','Veljači','Vitina','Vojnici','Zvirići','Bijača','Vitaljina',
+'Žabar','Prolog','Klobuk','Šipovača','Dole','Crnici','Doljani','Dračevo','Gabela','Gorica','Grabovina','Jasenica','Sjekose','Višići','Krupa','Gabela Polje','Čeljevo',
+'Struge','Babin Do','Borut','Brestica','Cerovo','Dobri Do','Dobrovo','Donji Zelenikovac','Dubravica','Duži','Glumina','Gradac','Hotonj Hutovski','Kiševo','Moševići',
+'Neum','Previš','Žukovica','Batkovići','Broćanac','Brštanica','Cerovica','Crnoglav','Dračevice','Donji Drijen','Gornji Drijen','Gornji Zelenikovac','Grabovica',
+'Gornje Hrasno','Hutovo','Hrasno','Nino Polje','Jazine','Kolojanj','Međugorje','Međuljuće','Mramor','Nerade','Podžablje','Prapratnica','Potkula','Osječanica','Oskorušica',
+'Rabrani','Radež','Radetići','Vinine','Vodeni Do','Vlaka','Vranjevo Selo','Žukovica','Rep','Belenići','Bobovišta','Cicrina','Čopice','Glavska','Golubinac','Grebci',
+'Ivanića','Kaluđurevići','Kijev Do','Kutina','Nevada','Orah','Orahov Do','Podosoje','Prosjek','Slavogostići','Slivnica Bobani','Slivnica Površ','Sparožići','Ščenica Bobani',
+'Trebimlja','Uskoplje','Vlaka','Začula','Zagradinje','Zaplanik','Vukovići','Trnčina','Turkovići','Baonine','Grab','Kunja Glavica','Lapja','Lušnica','Ljubovo','Mrnjići',
+'Ograde','Podštirovnik','Poljice Čičevo','Tuli','Ubla','Velja Gora','Zgonjevo','Kožen Do','Jablan Do','Uvijeća','Kremeni Do','Sedlari','Grmljani','Mareva Ljut'];
+
+var hrvmalim = hrvatska.map(hrvatska => hrvatska.toLowerCase());
+var bihmalim = bih.map(bih => bih.toLowerCase());
+var nalazi = false;
+
+
+
+function polje(poljeUpisa) {
+    document.getElementById(poljeUpisa).value = document.getElementById(poljeUpisa).value.toLowerCase();
+    for (var i = 0; i < hrvmalim.length; i++) {
+        if (document.getElementById(poljeUpisa).value === hrvmalim[i]) {
+		    nalazi = true;
+	        alert("Upisano mjesto se nalazi u pograničnom području!");
+		    break;
+	    }
+	}
+    if (nalazi === false) {
+        alert("Upisano mjesto nije u pograničnom području!");
+    }
+	
+}
+
+function polje2(poljeUpisa2) {
+    document.getElementById(poljeUpisa2).value = document.getElementById(poljeUpisa2).value.toLowerCase();
+    for (var i = 0; i < bihmalim.length; i++) {
+        if (document.getElementById(poljeUpisa2).value === bihmalim[i]) {
+		    nalazi = true;
+	        alert("Upisano mjesto se nalazi u pograničnom području!");
+		    break;
+	    }
+	}
+    if (nalazi === false) {
+      alert("Upisano mjesto nije u pograničnom području!");
+    }
+	
+}
